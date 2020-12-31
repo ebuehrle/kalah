e437a899cab92d78a77343f0e96fb7df6b0d9bb5
Etienne Bührle <43623224+ebuehrle@users.noreply.github.com>
2020-12-31 12:30:34 +0100

    initial commit

---
+++ online/boardview.js
@@ -0,0 +1,96 @@
+class BoardView {
+    constructor(stoneViews, houses, stores, boardState=Array(12).fill(4).concat([0, 0])) {
+        this.elementTransitionDuration = 0.5; // from CSS
+        this.elementTransitionDelay = 0.1; // in s
+
+        this.stoneViews = stoneViews;
+        this.houses = houses;
+        this.stores = stores;
+
+        let stoneIndex = 0;
+        this.state = boardState.map(numStones => {
+           stoneIndex += numStones;
+           return this.stoneViews.slice(stoneIndex - numStones, stoneIndex);
+        });
+    }
+
+    _transition(boardState) {
+        let freeStones = [];
+        let pickupSlot = -1;
+
+        boardState.forEach((numStones, slotIdx) => {
+            const numPickup = Math.max(0, this.state[slotIdx].length - numStones);
+            const pickup = this.state[slotIdx].splice(this.state[slotIdx].length - numPickup, numPickup);
+            freeStones.push(...pickup.reverse());
+
+            if (pickup.length && pickupSlot === -1) {
+                pickupSlot = slotIdx;
+            }
+        });
+
+        pickupSlot = (pickupSlot !== -1) ? pickupSlot : 0;
+        let totalDelay = 0;
+        for (let i = 0; i < boardState.length; i++) {
+            const slotIdx = (pickupSlot + i) % boardState.length;
+            const numStones = boardState[slotIdx];
+
+            let distribute = freeStones.splice(0, Math.max(0, numStones - this.state[slotIdx].length));
+            this.state[slotIdx].push(...distribute);
+
+            distribute.forEach(stoneView => {
+                stoneView.real.style.transitionDelay = `${totalDelay}s`;
+                totalDelay += this.elementTransitionDelay;
+            })
+        }
+
+        const animationTime = totalDelay + (totalDelay ? this.elementTransitionDuration : 0);
+        return animationTime;
+    }
+
+    _layout() {
+        const slots = [
+            ...this.houses,
+            ...this.stores,
+        ];
+        this.state.forEach((slot, i) => {
+            slot.forEach(stoneView => slots[i].appendChild(stoneView.dummy));
+        });
+    }
+
+    render(boardState) {
+        const animationTime = boardState ? this._transition(boardState) : 0;
+
+        this._layout();
+
+        this.state.forEach(slot => {
+            slot.forEach(stoneView => {
+                stoneView.update();
+            });
+        });
+
+        return new Promise(resolve => {
+            setTimeout(() => resolve(animationTime), animationTime * 1000);
+        });
+    }
+
+    activePlayer(player) {
+        const player0Store = this.stores[0];
+        const player1Store = this.stores[1];
+        const player0Houses = [0, 1, 2, 3, 4, 5].map(i => this.houses[i]);
+        const player1Houses = [6, 7, 8, 9, 10, 11].map(i => this.houses[i]);
+
+        if (player === 0) {
+            player0Store.classList.add('active');
+            player0Houses.forEach(h => h.classList.add('active'));
+            player1Store.classList.remove('active');
+            player1Houses.forEach(h => h.classList.remove('active'));
+        } else {
+            player1Store.classList.add('active');
+            player1Houses.forEach(h => h.classList.add('active'));
+            player0Store.classList.remove('active');
+            player0Houses.forEach(h => h.classList.remove('active'));
+        }
+    }
+}
+
+// export { BoardView };
---
+++ online/kalaha.js
@@ -0,0 +1,78 @@
+// import { Board } from './board.js';
+// import { BoardView } from './boardview.js';
+// import { StoneView } from './stoneview.js';
+  
+const dummyStones = Array.from(document.querySelectorAll('.stone.dummy'));
+const realStones = Array.from(document.querySelectorAll('.stone.real'));
+const houses = Array.from(document.querySelectorAll('.house'));
+const stores = Array.from(document.querySelectorAll('.store'));
+
+const p0Name = document.querySelector('.player0');
+const p1Name = document.querySelector('.player1');
+const messageText = document.querySelector('.message .text');
+const refreshButton = document.querySelector('button.refresh');
+
+realStones.forEach(s => s.classList.add([
+    'red', 'green', 'yellow',
+    'blue', 'purple', 'cyan'
+][Math.floor(Math.random() * 6)]));
+
+const stoneViews = realStones.map((realStone, i) => new StoneView(dummyStones[i], realStone));
+let boardView = new BoardView(stoneViews, houses, stores);
+
+window.addEventListener('resize', () => boardView.render());
+
+function activePlayer(player) {
+    boardView.activePlayer(player);
+    messageText.innerHTML = `${player ? p1Name.innerHTML : p0Name.innerHTML}, your turn.`
+}
+
+function resetGame(board=new Board(), player=0) {
+    gameState = {
+        board: board,
+        player: player,
+    };
+    activePlayer(gameState.player);
+    boardView.render(gameState.board.state);
+}
+
+resetGame();
+history.pushState(gameState, document.title);
+
+houses.forEach((houseView, slotIdx) => {
+    houseView.addEventListener('click', _ => {
+        const moveResult = gameState.board.move(slotIdx, gameState.player);
+        if (moveResult === null) {
+            return; // invalid move
+        }
+
+        const [boardDistribute, boardPickup] = moveResult;
+        boardView.render(boardDistribute.state).then(() => boardView.render(boardPickup.state));
+
+        gameState.board = boardPickup;
+        gameState.player = (gameState.player + 1) % 2;
+        history.pushState(gameState, document.title);
+        activePlayer(gameState.player);
+
+        if (!gameState.board.canMove(gameState.player)) {
+            const p0Score = gameState.board.playerScore(0);
+            const p1Score = gameState.board.playerScore(1);
+            if (p0Score > p1Score) {
+                messageText.innerHTML = `${p0Name.innerHTML} wins with ${p0Score} &mdash; ${p1Score}.`;
+            } else if (p1Score > p0Score) {
+                messageText.innerHTML = `${p1Name.innerHTML} wins with ${p1Score} &mdash; ${p0Score}.`;
+            } else {
+                messageText.innerHTML = `The game is drawn at ${p0Score} each! Another one?`;
+            }
+        }
+    });
+});
+
+document.querySelector('button.refresh').addEventListener('click', () => {
+    resetGame();
+    history.pushState(gameState, document.title);
+});
+
+window.addEventListener('popstate', ev => {
+    resetGame(new Board(ev.state.board.state), ev.state.player);
+});
